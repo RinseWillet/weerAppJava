114a39c192997e5434aeb22dab60e0300e6e9fd8
RinseWillet <r.willet@gmail.com>
2021-04-08 15:24:16 +0200

    "eerste commit"

---
+++ src/weerApp.java
@@ -0,0 +1,79 @@
+import java.net.URL;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+
+public class weerApp {
+
+    static String apiKey = "YOUR_API_KEY";
+    static String city = "den haag";
+
+    // Main Method die de data via een HTTP client (class weerData) gaat ophalen
+    public static void main(String[] args) {
+        String antwoord = "";
+
+        try {
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey);
+            weerData ht = new weerData(url);
+            //Connect to a HTTP url with "Connect Method", "User" and "Password" if required only.
+            String json = ht.httpConnect("", "GET", "user", "password");
+            System.out.println("lengte JSON : " + json.length());
+            antwoord = json.substring(0,(json.length()-1));
+            try {
+                weerDataParser parseData = new weerDataParser();
+                parseData.getData(antwoord);
+            } catch (Exception e) {
+                System.out.println("werkt niet");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //programma klaar
+        System.out.println("***********>>>> Tot zover het weerbericht <<<<**************");
+    }
+}
+
+//classe om de weer info uit de String te halen en in de terminal te printen
+class weerDataParser{
+
+    public boolean getData(String antwoord) throws Exception {
+
+        //JSON parser object aanmaken
+        JSONParser parser = new JSONParser();
+
+        try {
+
+            //met de parser het argument (antwoord) als string gaan parsen naar een JSONObject
+            JSONObject jsonObject = (JSONObject) parser.parse(antwoord);
+
+            //Welke stad
+            System.out.println("Stad : " + jsonObject.get("name"));
+
+            //algemene array uit object halen en printen
+            JSONArray tempData = (JSONArray) jsonObject.get("weather");
+            System.out.println("Weertype: " + ((JSONObject) tempData.get(0)).get("main"));
+            System.out.println("Beschrijving: " + ((JSONObject) tempData.get(0)).get("description"));
+
+            //details data uit object halen, in array zetten en vervolgens printen
+            JSONObject tempData1 = (JSONObject) jsonObject.get("main");
+            System.out.printf("Temperatuur: %.2f",  (Double.parseDouble(tempData1.get("temp").toString()) - 273.15)); //in Deg C
+            System.out.println(" graden Celsius.");
+            System.out.printf("Temperatuur Min: %.2f",(Double.parseDouble(tempData1.get("temp_min").toString()) - 273.15));//in Deg C
+            System.out.println(" graden Celsius.");
+            System.out.printf("Temperatuur Max: %.2f", (Double.parseDouble(tempData1.get("temp_max").toString()) - 273.15));//in Deg C
+            System.out.println(" graden Celsius.");
+            System.out.println("Luchtdruk: " + tempData1.get("pressure") + " hPa."); //hpa
+            System.out.println("Luchtvochtigheid: " + tempData1.get("humidity") + " %."); //%
+
+            //fouten catchen
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        //het is goed gegaan, true returnen
+        return true;
+    }
+}
+
