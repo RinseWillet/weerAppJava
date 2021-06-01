8e894c42568cf7080cf4cc19083756ddb99e412c
RinseWillet <r.willet@gmail.com>
2021-06-01 18:25:12 +0200

    - vereenvoudiging voor proeverij

--- src/weerApp.java
+++ src/weerApp.java
@@ -2,15 +2,21 @@ import java.net.URL;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class weerApp {
-
+    // string van de api key voor openweather
     static String apiKey = "9cbe90aaed00fc55a6bbf47a4b6c2678";
+
+    //default stad als er geen invoer is
     static String city = "Den+Bosch";
 
     // Main Method die de data via een HTTP client (class weerData) gaat ophalen
     public static void main(String[] args) {
+
+        //In deze string gaat de JSON
         String antwoord = "";
 
         String city = "Den+Bosch";
@@ -23,12 +29,32 @@ public class weerApp {
         String stad = input.replaceAll(" ", "+").toLowerCase();
 
         try {
+
+            //hier maken we een URL object aan met daarin het https adres string (plus de stad en de apiKey strings)
             URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + stad + "&appid=" + apiKey);
+
+
             weerData ht = new weerData(url);
-            //Connect to a HTTP url with "Connect Method", "User" and "Password" if required only.
-            String json = ht.httpConnect("", "GET", "user", "password");
+            //hier wordt een string gevuld met een JSON antwoord door de httpConnect functie op te roepen in het aangemaakte weerData object
+            String jsonAntwoord = ht.httpConnect("", "GET");
+
+            //het rauwe antwoord
+            System.out.println("nog niks mee gedaan : " + jsonAntwoord);
+
+            //lengte van deze string json
 //            System.out.println("lengte JSON : " + json.length());
-            antwoord = json.substring(0,(json.length()-1));
+
+            //hier wordt het allerlaatste karakter eraf gehaald
+            antwoord = jsonAntwoord.substring(0,(jsonAntwoord.length()-1));
+//            System.out.println("ff gesubstringt : " + antwoord);
+
+            System.out.println("indexgetal : " + antwoord.indexOf("name"));
+            System.out.println(antwoord.substring(antwoord.indexOf("name")));
+
+
+
+
+
             try {
                 weerDataParser parseData = new weerDataParser();
                 parseData.getData(antwoord);
@@ -41,7 +67,6 @@ public class weerApp {
         //programma klaar
         System.out.println("***********>>>> Tot zover het weerbericht <<<<**************");
     }
-
 }
 
 //classe om de weer info uit de String te halen en in de terminal te printen
--- src/weerData.java
+++ src/weerData.java
@@ -1,137 +1,79 @@
-import java.io.*;
-import java.net.*;
+import java.io.InputStream;
 import java.io.StringWriter;
-import java.util.Base64;
-import java.util.Base64.*;
-import javax.net.ssl.*;
+import java.io.IOException;
+import java.net.URL;
+import java.net.HttpURLConnection;
 
 //classe om een URL te parsen - voorbeeld naar http://javaonlinelessons.com/misc/how-to-get-weather-data-using-java/
 public final class weerData {
 
-    private String auth = null;
+    //URL classe is final (uit java.net.*)
     private URL httpurl = null;
 
-    public weerData (URL httpurl_) {
+    public weerData(URL httpurl_) {
         httpurl = httpurl_;
     }
 
-    public String httpConnect(String outStr, String connectMethod, String userNm,
-                              String pass) throws Exception {
+    public String httpConnect(String outStr, String connectMethod) throws Exception {
 
+        //geef aan dat je in de methode zit
         System.out.println("Starting HTTP Request....");
-        //Start een nieuwe HttpURLConnection
+
+        //Start een nieuwe HttpURLConnection (uit java.net.*) waarbij je het URL object cast als een HttpURLConnection en de openConnection methode gebruikt van het URL object
         HttpURLConnection con = (HttpURLConnection) httpurl.openConnection();
 
-        // Authorizatie krijgen
-        if (auth != null) {
-            con.setRequestProperty("Authorization", "Basic " + auth);
-        }
+        //Connectie krijgen met de connect() methode uit het HttpURLConnection object 'con'
+        con.connect();
 
-        if (userNm == null) {
-            auth = null;
-        }
-        auth = userNm + ":" + ((pass == null) ? "" : pass);
-        Encoder encoder = Base64.getEncoder();
-        auth = encoder.encodeToString(auth.getBytes());
+        // Antwoord van de connectie krijgen met de getResponseCode() methode
+        System.out.println("Connection Response code:" + con.getResponseCode());
 
-        //Anonieme verbinding zonder certificaten
-        if (con instanceof HttpsURLConnection) {
-            HttpsURLConnection https_con = (HttpsURLConnection) con;
-            //Create dummy Hostname Verifier
-            https_con.setHostnameVerifier(new HostnameVerifier() {
-                public boolean verify(String nm, SSLSession session) {
-                    return true;
-                }
-            });
-        }
-
-        //connectie parameters instellen
-        con.setInstanceFollowRedirects(false);
-        con.setDoOutput(true);
-        con.setDoInput(true);
-        con.setUseCaches(false);
-        con.setAllowUserInteraction(true);
-        con.setRequestMethod(connectMethod.toUpperCase());
-
-        //Connectie antwoord krijgen
-        if (connectMethod.equals(connectMethod)) {
-            con.connect();
-            System.out.println("Connection Response code:" + con.getResponseCode());
-        }
-
-        if (connectMethod.equals("POST") || connectMethod.equals("PUT")) {
-            OutputStream out = con.getOutputStream();
-            out.write(outStr.getBytes(), 0, outStr.getBytes().length);
-            out.flush();
-
-            try {
-                out.close();
-            } catch (Exception e) {
-            }
-        }
 
         //Uitlezen HTTP antwoord.
-        StringWriter strWtr = new StringWriter();
-        InputStream instr = null;
+        StringWriter antwoordStringWriter = new StringWriter();
+        InputStream antwoord = null;
 
         try {
-            instr = con.getInputStream();
-            int rint = instr.read();
-            strWtr.write(rint);
-            while (rint > 0) {
-                rint = instr.read();
-                strWtr.write(rint);
+            //gebruik de methode getInputStream die in het HttpURLConnection object 'con' zit om de leash 'antwoord' van InputStream te koppelen. Een Inputstream is een stroom informatie die vloeit uit een open connectie (HTTP)
+            antwoord = con.getInputStream();
+
+            // je gebruikt nu de read() methode uit de InputStream 'antwoord' om een integer te vullen met de data die binnenkomt. De getallen in de integers staan gelijk aan ASCII codes voor tekens
+            int leesGetal = antwoord.read();
+
+            System.out.println("check this out : " + leesGetal);
+
+            //en iedere integer die binnenkomt gaan we wegschrijven in het StringWriter object 'antwoordStringWriter'.
+            // Een Stringwriter slaat een characterstroom op in een string buffer, die vervolgens gebruikt kan worden om een string te bouwen
+            // als je de write() methode gebruikt met een integer bijv. write(123) dan wordt een character met ASCII characternummer 123 weggeschreven (dat is "{" voor 123)
+            antwoordStringWriter.write(leesGetal);
+
+            //zolang er een getal groter dan nul uit de inputstream in de integer 'rint' gezet kan worden, blijf hetvolgende doen
+            while (leesGetal > 0) {
+                //hier laat je het getal dat binnenkomt uit de printen
+//                System.out.println("het getal : " + rint);
+
+                //hier laat je de inputstream met methode read() het character-integer uitlezen en in de integer rint zetten
+                leesGetal = antwoord.read();
+
+                // het lees getal uit en schrijf als character weg in de antwoordStringWriter
+                antwoordStringWriter.write(leesGetal);
             }
+            //vang fouten (exceptions) af
         } catch (Exception ex) {
+            // print de stacktrace
             ex.printStackTrace();
-
+            //print de responsecode, als die niet 200 is (= alles okay), dan een IOException throwen
             System.out.println("HTTP ResponseCode=" + con.getResponseCode());
-
-            if ((con.getResponseCode() / 100) != 2) {
+            if (con.getResponseCode() != 200) {
                 throw new IOException(con.getResponseMessage());
             }
-
         }
 
-        //Http antwoord uitprinten
-        System.out.println(strWtr.toString());
+        //Http antwoord uitprinten uit antwoordStringWriter
+//        System.out.println(antwoordStringWriter.toString());
 
-        try {
-            instr.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return strWtr.toString();
-    }
-
-    // Dummy Trust Manager for secured connection, it does
-    //not validate certificate chains
-    static {
-        try {
-            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-
-                public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-                }
-
-                public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-                }
-            }};
-
-            SSLContext sc = SSLContext.getInstance("SSL");
-
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            SSLSocketFactory sslSocketFactory = sc.getSocketFactory();
-
-            HttpsURLConnection.setDefaultSSLSocketFactory(sslSocketFactory);
-        } catch (Exception e) {
-            System.out.println("Error in trust manager...");
-            e.printStackTrace();
-        }
+        //return een String door de antwoordStringWriter met de methode toString() om te zetten tot String
+        return antwoordStringWriter.toString();
     }
 }
 
